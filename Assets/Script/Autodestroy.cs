b3dc8cd00eb29829fa938909b4ede7dc762660cb
josh-twitty <31357931+josh-twitty@users.noreply.github.com>
2017-10-07 16:01:06 -0400

    +Juice
    +Player can now shoot with the space bar. See comments in script Player.cs for details
    +Free sound assets added for firing and destroying cyllinders. (Adding sounds can be done by attaching an Audio Source to the object it's meant to play from, then controlling it with either a script or the built-in behaviors.)

---
+++ Assets/Script/Autodestroy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Autodestroy : MonoBehaviour {
+    /*Attach this class to a ParticleSystem game object in order to destroy it automatically upon completion of its lifetime.
+    This script includes an optional delay on deletion that can be set in the engine dev environment. Here that is used to ensure
+    that the explosion sound effect is not set short.*/
+    public float delay;
+	void Start () {
+        ParticleSystem parts = gameObject.GetComponent<ParticleSystem>();
+        Destroy(gameObject, parts.duration + parts.startLifetime + delay);
+	}
+
+}
